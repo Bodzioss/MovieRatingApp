849edbbf8878681355b50601be476ac8564f0978
Bodzioss <marcin_bogdanski1@wp.pl>
2022-11-15 22:09:46 +0100

    Scaffold database from existing schema

---
+++ movie_rating_app/Models/AspNetUser.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace movie_rating_app.Models
+{
+    public partial class AspNetUser
+    {
+        public AspNetUser()
+        {
+            AspNetUserClaims = new HashSet<AspNetUserClaim>();
+            AspNetUserLogins = new HashSet<AspNetUserLogin>();
+            AspNetUserTokens = new HashSet<AspNetUserToken>();
+            Reviews = new HashSet<Review>();
+            Movies = new HashSet<Movie>();
+            Roles = new HashSet<AspNetRole>();
+        }
+
+        public string Id { get; set; } = null!;
+        public string? UserName { get; set; }
+        public string? NormalizedUserName { get; set; }
+        public string? Email { get; set; }
+        public string? NormalizedEmail { get; set; }
+        public bool EmailConfirmed { get; set; }
+        public string? PasswordHash { get; set; }
+        public string? SecurityStamp { get; set; }
+        public string? ConcurrencyStamp { get; set; }
+        public string? PhoneNumber { get; set; }
+        public bool PhoneNumberConfirmed { get; set; }
+        public bool TwoFactorEnabled { get; set; }
+        public DateTimeOffset? LockoutEnd { get; set; }
+        public bool LockoutEnabled { get; set; }
+        public int AccessFailedCount { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Nationality { get; set; }
+
+        public virtual Nationality? NationalityNavigation { get; set; }
+        public virtual ICollection<AspNetUserClaim> AspNetUserClaims { get; set; }
+        public virtual ICollection<AspNetUserLogin> AspNetUserLogins { get; set; }
+        public virtual ICollection<AspNetUserToken> AspNetUserTokens { get; set; }
+        public virtual ICollection<Review> Reviews { get; set; }
+
+        public virtual ICollection<Movie> Movies { get; set; }
+        public virtual ICollection<AspNetRole> Roles { get; set; }
+    }
+}
---
+++ movie_rating_app/Models/Genre.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace movie_rating_app.Models
+{
+    public partial class Genre
+    {
+        public Genre()
+        {
+            Movies = new HashSet<Movie>();
+        }
+
+        public string Name { get; set; } = null!;
+
+        public virtual ICollection<Movie> Movies { get; set; }
+    }
+}
