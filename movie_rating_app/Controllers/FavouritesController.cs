686acce63d6a398bac7c380aee9e1fbf1c2cd337
Bodzioss <marcin_bogdanski1@wp.pl>
2022-11-20 22:04:07 +0100

    Add basic controllers with views

---
+++ movie_rating_app/Controllers/FavouritesController.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using movie_rating_app.Data;
+using movie_rating_app.Models;
+
+namespace movie_rating_app.Controllers
+{
+    public class FavouritesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FavouritesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Favourites
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Favourites.Include(f => f.Movie).Include(f => f.User);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Favourites/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Favourites == null)
+            {
+                return NotFound();
+            }
+
+            var favourite = await _context.Favourites
+                .Include(f => f.Movie)
+                .Include(f => f.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (favourite == null)
+            {
+                return NotFound();
+            }
+
+            return View(favourite);
+        }
+
+        // GET: Favourites/Create
+        public IActionResult Create()
+        {
+            ViewData["MovieId"] = new SelectList(_context.Movies, "Id", "Id");
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: Favourites/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("UserId,MovieId,Id")] Favourite favourite)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(favourite);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MovieId"] = new SelectList(_context.Movies, "Id", "Id", favourite.MovieId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", favourite.UserId);
+            return View(favourite);
+        }
+
+        // GET: Favourites/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Favourites == null)
+            {
+                return NotFound();
+            }
+
+            var favourite = await _context.Favourites.FindAsync(id);
+            if (favourite == null)
+            {
+                return NotFound();
+            }
+            ViewData["MovieId"] = new SelectList(_context.Movies, "Id", "Id", favourite.MovieId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", favourite.UserId);
+            return View(favourite);
+        }
+
+        // POST: Favourites/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("UserId,MovieId,Id")] Favourite favourite)
+        {
+            if (id != favourite.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(favourite);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FavouriteExists(favourite.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MovieId"] = new SelectList(_context.Movies, "Id", "Id", favourite.MovieId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", favourite.UserId);
+            return View(favourite);
+        }
+
+        // GET: Favourites/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Favourites == null)
+            {
+                return NotFound();
+            }
+
+            var favourite = await _context.Favourites
+                .Include(f => f.Movie)
+                .Include(f => f.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (favourite == null)
+            {
+                return NotFound();
+            }
+
+            return View(favourite);
+        }
+
+        // POST: Favourites/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Favourites == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Favourites'  is null.");
+            }
+            var favourite = await _context.Favourites.FindAsync(id);
+            if (favourite != null)
+            {
+                _context.Favourites.Remove(favourite);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FavouriteExists(int id)
+        {
+          return _context.Favourites.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ movie_rating_app/Controllers/MovieCreatorsController.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using movie_rating_app.Data;
+using movie_rating_app.Models;
+
+namespace movie_rating_app.Controllers
+{
+    public class MovieCreatorsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MovieCreatorsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: MovieCreators
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.MovieCreators.Include(m => m.Creator).Include(m => m.Movie);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: MovieCreators/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.MovieCreators == null)
+            {
+                return NotFound();
+            }
+
+            var movieCreator = await _context.MovieCreators
+                .Include(m => m.Creator)
+                .Include(m => m.Movie)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (movieCreator == null)
+            {
+                return NotFound();
+            }
+
+            return View(movieCreator);
+        }
+
+        // GET: MovieCreators/Create
+        public IActionResult Create()
+        {
+            ViewData["CreatorId"] = new SelectList(_context.Creators, "Id", "Id");
+            ViewData["MovieId"] = new SelectList(_context.Movies, "Id", "Id");
+            return View();
+        }
+
+        // POST: MovieCreators/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MovieId,CreatorId,Id")] MovieCreator movieCreator)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(movieCreator);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CreatorId"] = new SelectList(_context.Creators, "Id", "Id", movieCreator.CreatorId);
+            ViewData["MovieId"] = new SelectList(_context.Movies, "Id", "Id", movieCreator.MovieId);
+            return View(movieCreator);
+        }
+
+        // GET: MovieCreators/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.MovieCreators == null)
+            {
+                return NotFound();
+            }
+
+            var movieCreator = await _context.MovieCreators.FindAsync(id);
+            if (movieCreator == null)
+            {
+                return NotFound();
+            }
+            ViewData["CreatorId"] = new SelectList(_context.Creators, "Id", "Id", movieCreator.CreatorId);
+            ViewData["MovieId"] = new SelectList(_context.Movies, "Id", "Id", movieCreator.MovieId);
+            return View(movieCreator);
+        }
+
+        // POST: MovieCreators/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("MovieId,CreatorId,Id")] MovieCreator movieCreator)
+        {
+            if (id != movieCreator.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(movieCreator);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MovieCreatorExists(movieCreator.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CreatorId"] = new SelectList(_context.Creators, "Id", "Id", movieCreator.CreatorId);
+            ViewData["MovieId"] = new SelectList(_context.Movies, "Id", "Id", movieCreator.MovieId);
+            return View(movieCreator);
+        }
+
+        // GET: MovieCreators/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.MovieCreators == null)
+            {
+                return NotFound();
+            }
+
+            var movieCreator = await _context.MovieCreators
+                .Include(m => m.Creator)
+                .Include(m => m.Movie)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (movieCreator == null)
+            {
+                return NotFound();
+            }
+
+            return View(movieCreator);
+        }
+
+        // POST: MovieCreators/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.MovieCreators == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.MovieCreators'  is null.");
+            }
+            var movieCreator = await _context.MovieCreators.FindAsync(id);
+            if (movieCreator != null)
+            {
+                _context.MovieCreators.Remove(movieCreator);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MovieCreatorExists(int id)
+        {
+          return _context.MovieCreators.Any(e => e.Id == id);
+        }
+    }
+}
